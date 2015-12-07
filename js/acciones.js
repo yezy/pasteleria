cdaa6e705685d2764eb2cdecac3644ba00576df7
yezy <damcb164e39@gmail.com>
2015-12-07 16:45:16 -0600

    6
    proyecto

--- js/acciones.js
+++ js/acciones.js
@@ -6,13 +6,14 @@ $(document).ready(function(e) {
 $('.imagenpastel').on('tap', function(){
 	$.ajax({
 		type: "POST",
-		url: "http://192.168.1.82/proyecto/buscarpasteles.php",
+		url: "http://192.168.1.170/proyecto/buscarpasteles.php",
 		data: "pastel=" + $(this).attr('id'),
 		error: function()
 		{
-		 alert ("error en la conexio");	
+		 alert ("error en la conexion");	
 		},
 		success: function(respuesta){
+			 alert (respuesta);
 		  var pastel=JSON.parse(respuesta);
 		  alert (pastel.nombre_pastel);
 		  $('#nombre_pastel').html(pastel.nombre_pastel);
